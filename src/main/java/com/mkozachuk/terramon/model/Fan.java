74a04e160c262b85ba9849a2654559ccd36f35a4
Maksym <maksym.kozachuk@protonmail.com>
2021-04-04 18:23:40 +0200

    add Slf4j in Fan class, @Value fro defaultMonitoringDaley

--- src/main/java/com/mkozachuk/terramon/model/Fan.java
+++ src/main/java/com/mkozachuk/terramon/model/Fan.java
@@ -1,11 +1,11 @@
 package com.mkozachuk.terramon.model;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
+@Slf4j
 public class Fan {
-
     private boolean on;
-   private int rotationSpeedInPercent;
-
+    private int rotationSpeedInPercent;
 }
