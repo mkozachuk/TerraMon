aec87d9d6afacc23898ce69fd3fdedf10d464d3a
Maksym <maksym.kozachuk@protonmail.com>
2021-04-10 14:26:16 +0200

    Add SettingsController and TerraDataController tests

---
+++ src/test/java/com/mkozachuk/terramon/controller/SettingsControllerTest.java
@@ -0,0 +1,47 @@
+package com.mkozachuk.terramon.controller;
+
+import com.mkozachuk.terramon.model.Terrarium;
+import com.mkozachuk.terramon.service.AboutService;
+import com.mkozachuk.terramon.service.NoteService;
+import com.mkozachuk.terramon.service.TerraDataService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+class SettingsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Terrarium terrarium;
+
+    @MockBean
+    private AboutService aboutService;
+
+    @MockBean
+    private NoteService noteService;
+
+    @MockBean
+    private TerraDataService terraDataService;
+
+    @Test
+    void settingsPageTest() throws Exception {
+        mockMvc.perform(get("/settings"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("settings"))
+                .andExpect(content().string(
+                        containsString("Settings TerraMon")));
+    }
+
+}
---
+++ src/test/java/com/mkozachuk/terramon/controller/TerraDataControllerTest.java
@@ -0,0 +1,46 @@
+package com.mkozachuk.terramon.controller;
+
+import com.mkozachuk.terramon.model.Terrarium;
+import com.mkozachuk.terramon.service.AboutService;
+import com.mkozachuk.terramon.service.NoteService;
+import com.mkozachuk.terramon.service.TerraDataService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+class TerraDataControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Terrarium terrarium;
+
+    @MockBean
+    private AboutService aboutService;
+
+    @MockBean
+    private NoteService noteService;
+
+    @MockBean
+    private TerraDataService terraDataService;
+
+    @Test
+    void mainDataPageTest() throws Exception {
+        mockMvc.perform(get("/data"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("data"))
+                .andExpect(content().string(
+                        containsString("TerraMon Data")));
+    }
+}
