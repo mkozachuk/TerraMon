e4c318ece75105a9eef067a6306b4e72e7b4efe8
Maksym <maksym.kozachuk@protonmail.com>
2021-04-11 13:38:13 +0200

    corrected typos

--- src/main/java/com/mkozachuk/terramon/bot/messages/Callbacks.java
+++ src/main/java/com/mkozachuk/terramon/bot/messages/Callbacks.java
@@ -4,49 +4,58 @@ import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageTe
 
 public class Callbacks {
     
-    public EditMessageText powerOff(Long chat_id, int message_id) {
+    public EditMessageText powerOff(Long chatId, int messageId) {
         String answer = "Wait 25 seconds ⌛\nand unplug";
         EditMessageText new_message = new EditMessageText()
-                .setChatId(chat_id)
-                .setMessageId(message_id)
+                .setChatId(chatId)
+                .setMessageId(messageId)
                 .setText(answer);
         return new_message;
     }
 
-    public EditMessageText canceled(Long chat_id, int message_id) {
+    public EditMessageText canceled(Long chatId, int messageId) {
         String answer = "Power Off has been canceled";
         EditMessageText new_message = new EditMessageText()
-                .setChatId(chat_id)
-                .setMessageId(message_id)
+                .setChatId(chatId)
+                .setMessageId(messageId)
                 .setText(answer);
         return new_message;
     }
 
-    public EditMessageText addNewNote(Long chat_id, int message_id) {
+    public EditMessageText addNewNote(Long chatId, int messageId) {
         String answer = "Provide new note starting from \"Note:\"\n" +
                 "Example:\n" +
                 "Note: Important information that I would like to save";
         EditMessageText new_message = new EditMessageText()
-                .setChatId(chat_id)
-                .setMessageId(message_id)
+                .setChatId(chatId)
+                .setMessageId(messageId)
                 .setText(answer);
         return new_message;
     }
 
-    public EditMessageText lastNotes(Long chat_id, int message_id) {
+    public EditMessageText lastNotes(Long chatId, int messageId) {
         String answer = "Last 3 notes:";
         EditMessageText new_message = new EditMessageText()
-                .setChatId(chat_id)
-                .setMessageId(message_id)
+                .setChatId(chatId)
+                .setMessageId(messageId)
                 .setText(answer);
         return new_message;
     }
 
-    public EditMessageText export(Long chat_id, int message_id) {
+    public EditMessageText export(Long chatId, int messageId) {
         String answer = "Exporting...";
         EditMessageText new_message = new EditMessageText()
-                .setChatId(chat_id)
-                .setMessageId(message_id)
+                .setChatId(chatId)
+                .setMessageId(messageId)
+                .setText(answer);
+        return new_message;
+    }
+
+    public EditMessageText callData(String callData, Long chatId, int messageId) {
+        String answer = callData;
+        EditMessageText new_message = new EditMessageText()
+                .setChatId(chatId)
+                .setMessageId(messageId)
                 .setText(answer);
         return new_message;
     }
